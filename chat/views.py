951974125b57e735db3b9292936ec075a923e8f0
Roman Volodin <roman.volodin@gmail.com>
2022-02-28 19:41:31 +0300

    Remove unused code

--- chat/views.py
+++ chat/views.py
@@ -1,31 +1,8 @@
-from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from chat.models import User, Chat, Message
+from chat.models import User, Chat
 from chat.serializers import UserSerializer, ChatSerializer, MessageSerializer
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by("-created_at")
-    serializer_class = UserSerializer
-
-
-class ChatViewSet(viewsets.ModelViewSet):
-    queryset = Chat.objects.all()
-    serializer_class = ChatSerializer
-
-
-class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
-    serializer_class = MessageSerializer
-
-
-@api_view(["GET"])
-def list_users(request):
-    users = User.objects.all()
-    serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
 
 
 @api_view(["POST"])
--- project/urls.py
+++ project/urls.py
@@ -1,21 +1,8 @@
 from chat import views
-from django.contrib import admin
-from django.urls import include, path
-
-from rest_framework import routers
-
-
-router = routers.DefaultRouter()
-router.register(r"users", views.UserViewSet)
-router.register(r"chats", views.ChatViewSet)
-router.register(r"messages", views.MessageViewSet)
+from django.urls import path
 
 
 urlpatterns = [
-    path("", include(router.urls)),
-    path("admin/", admin.site.urls),
-    path("auth/", include("rest_framework.urls", namespace="rest_framework")),
-    path("users/get", views.list_users),
     path("users/add", views.create_user),
     path("chats/get", views.list_chats),
     path("chats/add", views.create_chat),
